3a65b08dfb6207f43a15a7d48617374eea865093
BurnyLlama <burnyllama@protonmail.com>
2023-08-27 20:18:42 +0200

    Basic UI done...

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # news-site
+
 Simple news site
+
+## Generate styles
+
+```bash
+mkdir -p static/styles
+sass --style=compressed --watch --update --no-source-map scss/:static/styles
+```
--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,5 +1,11 @@
 use rocket::Route;
+use rocket_dyn_templates::{context, Template};
+
+#[get("/")]
+fn landing() -> Template {
+    Template::render("landing", context! {})
+}
 
 pub fn get_all_routes() -> Vec<Route> {
-    routes![]
+    routes![landing]
 }
