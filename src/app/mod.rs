daa8945dde2bee4cac30822f636c4fbc2da761a5
BurnyLlama <burnyllama@protonmail.com>
2024-03-23 12:45:04 +0100

    Started working on a control panel.

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -16,6 +16,11 @@ fn editor() -> Template {
     Template::render("editor", context! {})
 }
 
+#[get("/control-panel")]
+fn control_panel() -> Template {
+    Template::render("control_panel", context! {})
+}
+
 pub fn get_all_routes() -> Vec<Route> {
-    routes![landing, about_us, editor]
+    routes![landing, about_us, editor, control_panel]
 }
