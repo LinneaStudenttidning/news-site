2146d2ecee8f6e101ba0d78e44dae7cf6057d75a
BurnyLlama <burnyllama@protonmail.com>
2024-04-03 22:54:06 +0200

    Started work on a more maintainable texteditor.

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -16,12 +16,16 @@ fn editor() -> Template {
     Template::render("editor", context! {})
 }
 
+#[get("/editor-v2")]
+fn editor_v2() -> Template {
+    Template::render("editor-v2", context! {})
+}
+
 #[get("/control-panel")]
 fn control_panel() -> Template {
     Template::render("control_panel", context! {})
 }
 
 pub fn get_all_routes() -> Vec<Route> {
-    routes![landing, about_us, editor, control_panel]
+    routes![landing, about_us, editor, editor_v2, control_panel]
 }
-
