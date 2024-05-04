4946c790a759a8a02f51bf2ffc5df7fbd84f3295
furo <furo@posteo.net>
2024-05-04 12:34:49 +0200

    Only show tags from published articles

--- sql/articles/get_all_tags.sql
+++ sql/articles/get_all_tags.sql
@@ -1,3 +1,3 @@
 SELECT ARRAY (SELECT tag FROM 
-    (SELECT UNNEST(tags) as tag FROM articles) 
+    (SELECT UNNEST(tags) as tag FROM articles WHERE is_published = true) 
 GROUP BY tag ORDER BY COUNT(*) DESC LIMIT $1);
